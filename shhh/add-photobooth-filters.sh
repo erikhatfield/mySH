b8fd0ff164c709a3cdbc313ac95dc791ce40b304
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2020-03-06 20:14:09 -0500

    Create add-photobooth-filters.sh

---
+++ shhh/add-photobooth-filters.sh
@@ -0,0 +1,106 @@
+# Consider dynamic OOP approach to cp'ing qtz files 030620~ELH
+
+# ls -a /System/Library/Compositions
+# Copy this list of files from /System to Squiggly/ (current user's directory)
+
+cp /System/Library/Compositions/Color\ Invert.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Exposure\ Adjust.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Kaleidescope.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Pointillize.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Twirl.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/ColorPencil.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Fall.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/LightTunnel.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/PopArt.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 1.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/ComicBook.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/False\ Color.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Line\ Overlay.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Posterize.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 2.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Compound\ Eye.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Film\ Stock.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Line\ Screen.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Push.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 3.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/ASCII\ Art.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Concert.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Fish.mov ~/Library/Compositions/
+cp /System/Library/Compositions/Lovestruck.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Rollercoaster.mov ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 4.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/BlackAndWhite.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Copy\ Machine.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Fish.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Mask.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Rollercoaster.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 5.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Blockhead.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Crystallize.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/FishEye.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Mirror.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Sepia.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 6.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Blue\ Print.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Cube.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Flip-Flop.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Mod.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Sharpen.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 7.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Blur.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Floor.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Monochrome.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Space\ Alien.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/User\ Backdrop\ 8.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Bug\ Out.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Defocus.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Frog.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Mosaic\ Flip.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Squeeze.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Wall.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Bulge.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Dent.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Gamma\ Adjust.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Motion\ Blur.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Stretch.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/XRay.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Chipmunk.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Dissolve.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Glow.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Moving\ Shapes.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Sunset.mov ~/Library/Compositions/
+cp /System/Library/Compositions/Yosemite.mov ~/Library/Compositions/
+cp /System/Library/Compositions/City\ Lights.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Dizzy.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Gradient.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Neon.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Sunset.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Yosemite.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/CloakingDevice.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Dot\ Screen.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Grid.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Nose\ Twirl.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Swing.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Zoom\ Blur.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Clouds.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Earthrise.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Hologram.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Pinch.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/ThermalCamera.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Zoom\ Dissolve.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Color\ Controls.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Eiffel\ Tower.mov ~/Library/Compositions/
+cp /System/Library/Compositions/Image\ Hose.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Pixellate.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Tracer.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Color\ Dots.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Eiffel\ Tower.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Image\ Resizer.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/PlasticCamera.qtz ~/Library/Compositions/
+cp /System/Library/Compositions/Travelator.qtz ~/Library/Compositions/
+
+
+# Search and replace the string 'PhotoBooth' with 'PhotoSpoof' in files in ~/Library/Compositions using perl
+cd ~/Library/Compositions
+perl -i -pe 's/PhotoBooth/PhotoSpoof/g' ./*
+
