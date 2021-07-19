8baaf6b9748a09a4df0cc3b74828acaa10619d2a
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2021-07-19 19:05:51 -0400

    Update search_and_destroy.sh

--- search_and_destroy.sh
+++ search_and_destroy.sh
@@ -10,8 +10,8 @@
 
 #howdoyouunderstandmenow
 
-#dry#find . -type f -exec md5sum {} + | awk '$1 == "eg1anmd5h4sh5tr5amp13101lolw00t" {printf "%s\0", substr($0, 35)}' | xargs -r0 -n1
-#wet#find . -type f -exec md5sum {} + | awk '$1 == "eg1anmd5h4sh5tr5amp13101lolw00t" {printf "%s\0", substr($0, 35)}' | xargs -r0 rm -rf
+#dry#find . -type f -exec md5 {} + | awk '$1 == "eg1anmd5h4sh5tr5amp13101lolw00t" {printf "%s\0", substr($0, 35)}' | xargs -r0 -n1
+#wet#find . -type f -exec md5 {} + | awk '$1 == "eg1anmd5h4sh5tr5amp13101lolw00t" {printf "%s\0", substr($0, 35)}' | xargs -r0 rm -rf
 
 #awk '{printf "%s%s", NR-1 ? "|" : "", $1}' blash-list.txt will reformat this into a single line of pipe | separated hashes.
 
