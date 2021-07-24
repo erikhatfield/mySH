c38978c2889dc4b1d05d3c620bdf37e7a11af613
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2021-07-24 19:32:13 -0400

    Update for_each_arguement.sh

--- for_each_arguement.sh
+++ for_each_arguement.sh
@@ -4,9 +4,11 @@
 ## instructions: ##
 ## run both of these lines on the command line from within the containing directory.
 #% chmod u+x for_each_arguement.sh
-#% ./for_each_arguement.sh "arguement one" "argument ii" "arguement 3"
+#% ./for_each_arguement.sh "arguement one" "argument ii"
 
-for arguement in "$@"
+for filepath in "$@"
 do
-    echo "$arguement"
+    echo "$filepath"
+
+    md5 -r "$filepath" >> ~/Desktop/blash-list.txt
 done
