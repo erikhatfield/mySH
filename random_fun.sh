213cecb0bb9927de5875701ced4dbfb35dda8ee4
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2021-07-29 21:33:44 -0400

    random update
    with random details

--- random_fun.sh
+++ random_fun.sh
@@ -19,16 +19,16 @@
 # generate random number and store in variable 
 rand=$RANDOM
 
-# echo a random integer <= 325
+echo "a random integer <= 325"
 echo $(( rand %= 325 ))
 # difference in operators? mod equals vs. just mod -wondering: <= vs. <
 echo $(( rand % 325 ))
 
-# echo random number between 325 and 25 (excitiiiiing)
+echo "random number between 325 and 25 (excitiiiiing)"
 echo $(( rand % 325 + 25 ))
 
-# echo 5 random ints in said range (with spiffy one-line for loop)
-for i in {1..5}; do echo $(( RANDOM % 325 + 25 )); done
+echo "5 random ints in said range (with spiffy one-line for loop)"
+## for i in {1..5}; do echo $(( RANDOM % 325 + 25 )); done
 # same for loop in easier-to-read format (for shell script)
 for i in {1..5}
   do
@@ -42,3 +42,16 @@ for ((i=1; i<=$(( RANDOM % 7 + 3 )); i++))
   do
     echo $(( RANDOM % 325 + 25 ))
 done
+
+#############################################################
+## RANDOM_NOTE2SELF: how to spell the word 'environment'   ##
+## env iron ment                                           ##
+#############################################################
+# print env variables
+env
+
+# random:
+# 2 ways to exit the vi/vim editor
+# first: open another terminal window and type 'man vim' to read exit instructions
+## second: type the 'esc' key a random number of times (at least twice), then type the three characters :q! and hit 'enter'
+## exit vim: 'esc' :q! 'enter'
