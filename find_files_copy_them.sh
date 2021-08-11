635bb4b8050053f70e38194aeed0d44b74141ae6
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2021-08-11 01:25:14 -0400

    Update find_files_copy_them.sh

--- find_files_copy_them.sh
+++ find_files_copy_them.sh
@@ -6,20 +6,33 @@
 ## If they have duplicate file names, do not overwrite (default unix behaviour?)
 ## 
 ## Run Instructions: 2 arguements passed in to script as such:
-## arguement $1: file/path/to/start/search/
-## arguement $2: "*search.string" with any *wildcards* included
+## arguement $1: "*search.string"; note: include any *wildcards* in string
+## arguement $2: file/path/to/start/search/
+
+# if no arguements provided, use defaults
+# defaults:  SEARCH_STRING = "*.blend"; SEARCH_FILEPATH = current dir ".";
+SEARCH_STRING="${1:=*.blend}"
+SEARCH_FILEPATH="${2:=.}"  # If arguement $1 not provided, set it to the current dir "."
 
 ###################
-echo 'in the current directory "." find and print all filepaths that are of type f (file) and have a filename that ends with ".blend"' && sleep 1
-find . -name "*.blend" -type f  -print
+#### Early Dev Test
+##echo 'in the current directory "." find and print all filepaths that are of type f (file) and have a filename that ends with ".blend"' && sleep 1
+##find . -name "*.blend" -type f  -print
 
+###################
+echo 'in the directory '$SEARCH_FILEPATH' find and print all filepaths that are of type f (file) and have a filename with the pattern "'$SEARCH_STRING'"' && sleep 1
+find $SEARCH_FILEPATH -name "$SEARCH_STRING" -type f  -print
+echo '' && echo '' && sleep 1
+###################
+
+###################
 ## create a temp dir on the Desktop, using today's date
 date_temp_dir=$(date +"%m%d%y")
 mkdir "~/Desktop/temp"$date_temp_dir
 
 ## save output to file named with the current hour hh and minutes mm
 date_temp_file=$(date +"%H%M")
-find . -name "*.blend" -type f  -print >> "~/Desktop/temp"$date_temp_dir"cpthese_"$date_temp_file".txt"
+find $SEARCH_FILEPATH -name "$SEARCH_STRING" -type f  -print >> "~/Desktop/temp"$date_temp_dir"/cpthese_"$date_temp_file".txt"
 
 ###################
 ############################################################################
@@ -30,10 +43,6 @@ find . -name "*.blend" -type f  -print >> "~/Desktop/temp"$date_temp_dir"cpthese
 ###################
 ############################################################################
 ###################
-############################################################################
-###################
-############################################################################
-## code dumped from l box needs work
 
 while read -r eachline; do
 
@@ -41,16 +50,16 @@ while read -r eachline; do
       #filepath4operation=$(printf "%s" "$eachline" | find . -type f -exec md5 -r {} + | grep "$eachline" | cut -d ' ' -f2- )
 
       filepath4operation = $eachline
-      echo $filepath4operation
       
       if [ -z "$filepath4operation" ]
       then
           echo "\$filepath4operation is empty"
       else
-            
+          
+            echo $filepath4operation
 
       fi
 
-done < ~/Desktop/tempfilelawlz.txt
+done < "~/Desktop/temp"$date_temp_dir"/cpthese_"$date_temp_file".txt"
 
 
