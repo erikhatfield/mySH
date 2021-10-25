3a1ba1eb579ec95f160a60c4c607d7c0f702c396
erikhatfield <2522870+erikhatfield@users.noreply.github.com>
2021-10-25 16:32:28 -0400

    added if statement syntax/bracket comments

--- if.sh
+++ if.sh
@@ -4,13 +4,14 @@
 # IF statements                                                               #
 ###############################################################################
 
+# Comparing string variables specific IF statement syntax/brackets
 string_variable="Y"
 if [ $string_variable == "Y" ]; then
 			echo "If Statement: String comparision"
 else
 			echo ""
 fi
-
+# IF statements with integer operations require different syntax/brackets  than string comparisions
 int_variable=14
 if (( $int_variable <= 14 )); then
 			echo "If Statement: Integer comparision"
