263bab4249551eaf3d37cc69ad495cdda62b82b4
erikhatfield <2522870+erikhatfield@users.noreply.github.com>
2021-10-28 14:25:51 -0400

    added basic string examples and descriptives

--- basics_first.sh
+++ basics_first.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-###################
-## instructions: ##
-## run both of these lines on the command line from within the containing directory.
-#% chmod u+x basics_first.sh 
-#% ./basics_first.sh "the data passed into this shell script."
+#########################
+## basic instructions: ##
+## run both of these lines on the command line from within the containment directory.
+#% chmod u+x basics_first.sh
+#% sh ./basics_first.sh "the data passed into this shell script."
 
 FIRST_ARGUMENT="${1?parameter missing - include an arguement when running this shell.}"
 echo "The programming definition of an 'argument' is $FIRST_ARGUMENT!"
+echo
+echo
+############################
+## Basic variable syntax: ##
+BASIC_STRING_VARIABLE_WITH_LONG_DESCRIPTOR_TITLE="Defining dot SH string variable syntax cannot include white space outside of quotes."
+BASIC_STRING_VARIABLE_WITH_LONG_DESCRIPTOR_TITLE=$BASIC_STRING_VARIABLE_WITH_LONG_DESCRIPTOR_TITLE" The string must include quotes if white space is present in string."
+echo "Echoing basic string variable on next line:"
+echo $BASIC_STRING_VARIABLE_WITH_LONG_DESCRIPTOR_TITLE
+echo
+BASIC_STR="Basic string"
+echo $BASIC_STR" variables can be referenced from within/outside quotes, and even inside secondary quotes, e.g. '$BASIC_STR'"
+echo
