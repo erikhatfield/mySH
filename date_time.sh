6a3642ac7f3bf27855a97856b137eb7ba1d51488
erikhatfield <2522870+erikhatfield@users.noreply.github.com>
2021-11-02 11:46:38 -0400

    added time stamp calculation

--- date_time.sh
+++ date_time.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+########
+# Date #
+########
+# Record initial time stamp (in seconds since 010170 - January 1st, 1970 @0000)
+initialTimeStamp=$(date +"%s")
+echo $initialTimeStamp
+echo
+sleep 1
 #Today's date and time mmddyy@hhmm
 echo "Today's date and @time is "$(date +"%m%d%y @%H%M")
+echo
+sleep 1
+# Save a progressed time stamp and compare (subtraction operation)
+progressingTimeStamp=$(date +"%s")
+echo $(( progressingTimeStamp - initialTimeStamp ))" seconds have elapsed since initial time stamp"
+echo
 
