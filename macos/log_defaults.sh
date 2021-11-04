c6dd84ee30c0e5b4a9633af8eb2bef27608ee1f2
erikhatfield <2522870+erikhatfield@users.noreply.github.com>
2021-11-04 13:02:17 -0400

    Update log_defaults.sh
    Added subsection readout for safari defaults

--- macos/log_defaults.sh
+++ macos/log_defaults.sh
@@ -20,14 +20,36 @@ home_path="$HOME"
 
 # Make dir for output of defaults
 mkdir -p $home_path/Dev/defaults
-
+com.apple.Safari
 # Output defaults to filename with date and time designation
 FILENAME_FOR_DEFAULTS='read_'$(date +"%m%d%y")'_'$(date +'%H%M')'.txt'
 defaults read > $home_path/Dev/defaults/$FILENAME_FOR_DEFAULTS
-# Overwrite for git/diff
-##defaults read > ~/Dev/defaults/compare.txt
+# remove previous txts (if they exist)
+rm -rf $home_path/Dev/defaults/previous.txt
+# save new previous (if compare exists)
+mv $home_path/Dev/defaults/compare.txt $home_path/Dev/defaults/previous.txt
+# write new compare.txt for git/diff
 defaults read > $home_path/Dev/defaults/compare.txt
 
 echo "defaults logged @ $home_path/Dev/defaults/$FILENAME_FOR_DEFAULTS"
+echo
+echo "use## diff --side-by-side --suppress-common-lines $home_path/Dev/defaults/previous.txt $home_path/Dev/defaults/compare.txt"
+echo
+echo "Comparing: "
+#diff -Iy $home_path/Dev/defaults/previous.txt $home_path/Dev/defaults/compare.txt
+diff --side-by-side --suppress-common-lines $home_path/Dev/defaults/previous.txt $home_path/Dev/defaults/compare.txt
+sleep 4
+echo
+
+# do the same for com.apple.Safari
+# remove previous txts (if they exist)
+rm -rf $home_path/Dev/defaults/safari_previous.txt
+# save new previous (if safari_current exists)
+mv $home_path/Dev/defaults/safari_current.txt $home_path/Dev/defaults/safari_previous.txt
+# write new safari_current.txt for git/diff
+defaults read com.apple.Safari > $home_path/Dev/defaults/safari_current.txt
+# diff
+echo 'for safari diff use: '
+echo 'diff --side-by-side --suppress-common-lines '$home_path'/Dev/defaults/safari_previous.txt '$home_path'/Dev/defaults/safari_current.txt'
 
 #exit
