328e9491349af776d5e9b5d847cc9d5c8d97d4bf
erikhatfield <2522870+erikhatfield@users.noreply.github.com>
2023-07-01 02:04:12 -0700

    updated src + perms

--- macos/art_alias_setup.sh
+++ macos/art_alias_setup.sh
@@ -1,19 +1,16 @@
-# bin bish bash zsh ! from template
-# add alias for art 
-# perhaps check to see if it exists? that is, check to see if line u about to add exists already in:
-# ~/.bash_profile
-# and
-# ~/.zshrc
-# or
-# just (first phase)
-# add it to both 
-# ~/.bash_profile and ~/.zshrc
-# like so:
-echo 'alias art="~/Dev/BL/art-from-code/SH/art-from-code.sh"' >> ~/.bash_profile
-echo 'alias art="~/Dev/BL/art-from-code/SH/art-from-code.sh"' >> ~/.zshrc
+#!/bin/sh
+
+###################
+# add alias for art
+
+# maybe later: check to see if it exists? that is, check to see if line u about to add exists already in:
+
+echo 'alias art="~/Art/BL/art-from-code/SH/art-from-code.sh"' >> ~/.bash_profile
+echo 'alias art="~/Art/BL/art-from-code/SH/art-from-code.sh"' >> ~/.zshrc
+
 # and then source both of them (so that it may work again within future terminal wins)
 source ~/.bash_profile
 source ~/.zshrc
-# then final step of phase 1, test it
-art
 
+# then final step of phase 1, test it
+echo 'test it with alias => art'
