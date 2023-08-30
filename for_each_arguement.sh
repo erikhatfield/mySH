a3f975463def5910d25a011000eb9328c78053a6
i59 <i59@localhost>
2023-08-29 23:01:42 -0700

    added relative path for txt file

--- for_each_arguement.sh
+++ for_each_arguement.sh
@@ -6,9 +6,24 @@
 #% chmod u+x for_each_arguement.sh
 #% ./for_each_arguement.sh "arguement one" "argument ii"
 
+#######################################################
+# setup and use a relative path for blash-list txt file
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+relative_path=$(realpath "$0")
+relative_dir=$(dirname $relative_path)
+echo $relative_dir
+
+###############################
+# iterate thru params handed in
 for filepath in "$@"
 do
-    echo "$filepath"
-
-    md5 -q "$filepath" >> ~/Desktop/blash-list.txt
+    echo "working with filepath: $filepath"
+    #md5 -q "$filepath" >> ~/Desktop/blash-list.txt
+    #md5 -q "$filepath" >> $relative_dir/blash-list.txt
+    cd $relative_dir
+    cd ../RUN
+    echo "Saving blash-list in dir @"$(pwd)
+    md5 -q "$filepath" >> ./blash-list.txt
 done
