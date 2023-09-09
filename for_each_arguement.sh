ae6b5924556af6b6fec980f72f8192135bbff248
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2023-09-09 16:35:54 -0400

    Update for_each_arguement.sh
    testing on linux turns out relative dir syntax requires bash (not sh)

--- for_each_arguement.sh
+++ for_each_arguement.sh
@@ -1,29 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-###################
-## instructions: ##
-## run both of these lines on the command line from within the containing directory.
-#% chmod u+x for_each_arguement.sh
+################
+### instructions 
+##ensure exec privs
+#% chmod u+x ./for_each_arguement.sh
+##use bash
 #% ./for_each_arguement.sh "arguement one" "argument ii"
+################
 
 #######################################################
 # setup and use a relative path for blash-list txt file
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
-relative_path=$(realpath "$0")
+relative_path=$(realpath "$0") #contains filename.ext
 relative_dir=$(dirname $relative_path)
-echo $relative_dir
+echo "Working directory: "$relative_dir
+cd $relative_dir #might fail if spaces in path?
 
 ###############################
 # iterate thru params handed in
 for filepath in "$@"
 do
-    echo "working with filepath: $filepath"
+    echo "operating on file @ filepath: $filepath"
     #md5 -q "$filepath" >> ~/Desktop/blash-list.txt
     #md5 -q "$filepath" >> $relative_dir/blash-list.txt
-    cd $relative_dir
-    cd ../RUN
+    
     echo "Saving blash-list in dir @"$(pwd)
     md5 -q "$filepath" >> ./blash-list.txt
 done
+#excitingggg stufffff... =P
