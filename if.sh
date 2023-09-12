94ddf965dcdd911349cc55a1bcf6fb7481b57de9
Erik Hatfield <2522870+erikhatfield@users.noreply.github.com>
2023-09-12 17:50:11 -0400

    added check for partial string
    in file

--- if.sh
+++ if.sh
@@ -8,39 +8,46 @@
 # Comparing string variables specific IF statement syntax/brackets
 string_variable="Y"
 if [ $string_variable == "Y" ]; then
-			echo "If Statement: String comparision"
+	echo "If Statement: String comparision"
 else
-			echo ""
+	echo ""
 fi
 
 ##############################
 # Checking for an empty string
 iamemptyorami=""
 if [ ! -z "$iamemptyorami" ]; then
-			echo "Guess I'm not empty"
+	echo "Guess I'm not empty"
 else
-			echo "Empty string is empty"
+	echo "Empty string is empty"
 fi;
 
 ###################################################################################################
 # IF statements with integer operations require different syntax/brackets  than string comparisions
 int_variable=14
 if (( $int_variable <= 14 )); then
-			echo "If Statement: Integer comparision"
+	echo "If Statement: Integer comparision"
 else
-			echo ""
+	echo ""
 fi
 
 ###############
 # check whereis
 whereistimbuckii=$(whereis timbuckii)
-
 if [ "$whereistimbuckii" = "timbuckii:" ]; then 
     echo "Sir, Tim Buck who sir?"
 else
     echo $whereistimbuckii  
 fi;
 
+################################
+# check for presence of a string
+ifEmptyNoMatch=$(cat ~/.zshrc | grep "alias art=") ###careful here, could be multiples
+if [ ! -z "$ifEmptyNoMatch" ]; then
+	echo "Oh thank God, †, we have art!"
+else
+	echo "Install alias art here"
+fi;
 
 #####
 #exit
