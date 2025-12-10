75fe4b6b4529a0e52b57eb441b0c8a784bea3cf3
standarddriver <standarddriver@local.host>
2025-12-10 12:05:58 -0800

    converted absolute path to relative

--- macos/alias_setup.sh
+++ macos/alias_setup.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+relative_path=$(realpath "$0")
+relative_dir=$(dirname $relative_path)
+echo $relative_dir && ls -ale $relative_dir
+
 ###################################################
 mkdir -p ~/Dev/shhh_local
-chmod 700 ~/Dev/shhh_local
+chmod -R 770 ~/Dev/shhh_local
 
+find ~/Dev/shhh_local -type f -iname "*.sh" -exec rm -rfv '{}' \;
 
 ###################################################
 # Recovery boot sh
 SH_FILE_NAME=rebootinR.sh
 ALIAS_STR='recovery="sh'
-sh /Volumes/D0TS/latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
-
+sh $relative_dir/../../latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
 
 ###################################################
 # add metadata sanitizer
 SH_FILE_NAME=sanitize_multiple_files.sh
 ALIAS_STR='sanitize="sh' 
-sh /Volumes/D0TS/latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
+sh $relative_dir/../../latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
 
 ##ALSO needs 2nd file:
 sudo chmod 777 ~/Dev/shhh_local/downloaded_files.sh && rm -rfv ~/Dev/shhh_local/downloaded_files.sh
-cp -a /Volumes/D0TS/latest/osx/profile/downloaded_files.sh ~/Dev/shhh_local/downloaded_files.sh
+cp -a $relative_dir/../../latest/osx/profile/downloaded_files.sh ~/Dev/shhh_local/downloaded_files.sh
 chmod 500 ~/Dev/shhh_local/downloaded_files.sh
 
 
@@ -28,16 +35,36 @@ chmod 500 ~/Dev/shhh_local/downloaded_files.sh
 # add csr_override example
 SH_FILE_NAME=csr_override.sh
 ALIAS_STR='csroverride="sh'
-sh /Volumes/D0TS/latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
+sh $relative_dir/../../latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
 
 
 #################################################
 # add inspect
 SH_FILE_NAME=inspecther.sh
 ALIAS_STR='inspecther="sh'
-sh /Volumes/D0TS/latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
+sh $relative_dir/../../latest/osx/profile/update_an_alias.sh $SH_FILE_NAME $ALIAS_STR
 
 
+#################################################
+# add J0URNAL
+sh $relative_dir/../../latest/SRS/delete_line_with_match.sh "J0URNAL" ~/.bash_profile
+sh $relative_dir/../../latest/SRS/delete_line_with_match.sh "J0URNAL" ~/.zshrc
+#removing from bash profiles will allow for updating script too
+sh $relative_dir/../../latest/J0URNAL/journal_installer.sh
+echo && echo
+cat ~/Dots/J0URNAL/entries/.log
+echo && sleep 1
+
+##########################
+# add others without aliases
+find $relative_dir/../../latest/osx/profile/MOAR4DEV+shhh_local -type f -iname "*.sh" -exec cp -a
+sudo cp -a $relative_dir/../../latest/osx/profile/MOAR4DEV+shhh_local/*.sh ~/Dev/shhh_local
+
+sudo sh ~/Dev/shhh_local/permsperpath.sh ~/Dev/shhh_local
+
+sleep 1 && echo
+sudo chmod 0700 ~/Dev/shhh_local/
+sudo chmod 0500 ~/Dev/shhh_local/*.sh
 
 ##########################################################################
 ##########################################################################
@@ -94,4 +121,5 @@ source ~/.zshrc
 
 # well thats all for now folks
 exit
+
 
